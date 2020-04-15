bd84058c978431d5bdf37896233b82f6b6e95ea6
Tomzing <Tomzing@users.noreply.github.com>
2020-04-15 18:34:23 +0200

    Implementerte captcha på registrering av forelesere

--- registrerForeleser.php
+++ registrerForeleser.php
@@ -8,6 +8,19 @@
 </head>
 <body>
 
+<script src="https://www.google.com/recaptcha/api.js?render=6LdUcegUAAAAAEaEyVtUMVUltxOTPsIGLg7GwdLA"></script>
+<script>
+    window.onload;
+
+    grecaptcha.ready(function() {
+        grecaptcha.execute('6LdUcegUAAAAAEaEyVtUMVUltxOTPsIGLg7GwdLA', {action: 'homepage'}).then(function(token) {
+            var recaptchaResponse = document.getElementById('recaptchaResponse');
+            recaptchaResponse.value = token;
+            console.log(recaptchaResponse.value);
+        });
+    });
+</script>
+
 <div id="wrapper">
 
     <h2 id="title">Registrer deg (bare for forelesere)</h2>
@@ -19,6 +32,7 @@
         <span id="#txtB">Epost</span><input  id="inputB" type="email" name="epost" required></input>
         <span>Et bilde av deg takk</span>
         <input type="file" name="file" id="fileToUpload">
+        <input type="hidden" name="recaptcha_response" id="recaptchaResponse">
         <button id="confirm" name="submit" type="submit">Bekreft</button> <a id="reg" href="hvemErDu.php">Tilbake</a>
     </form>
         
