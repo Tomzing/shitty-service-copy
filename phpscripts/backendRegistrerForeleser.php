d1952e1187227261d319948fbf6e011d4f8af8d3
betalfa <trymhm@gmail.com>
2020-04-16 18:08:42 +0200

    psueh for thomas

--- phpscripts/backendRegistrerForeleser.php
+++ phpscripts/backendRegistrerForeleser.php
@@ -1,7 +1,25 @@
 <?php
 include("connectionTable.php");
 header("Access-Control-Allow-Origin: *");
-lagCon("foreleser","ITyu8uXEVmXxA3iX");
+
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'foreleser';
+$DATABASE_PASS = 'ITyu8uXEVmXxA3iX';
+$DATABASE_NAME = 'virusnet';
+// Try and connect using the info above.
+$conMysqli = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+$conMysqli->set_charset("utf8");
+if (mysqli_connect_errno() ) {
+    // If there is an error with the connection, stop the script and display the error.
+    die ('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+
+$con = mysqli_connect("localhost", "root", "1337hackermangruppe09", "virusnet");
+define( 'DB_HOST', 'localhost' ); // set database host
+define( 'DB_USER', 'student' ); // set database user
+define( 'DB_PASS', 'IA1vz6TNpdya6X8G' ); // set database password
+define( 'DB_NAME', 'virusnet' ); // set database name
+define( 'DISPLAY_DEBUG', false ); //display db errors?
 
 // Build POST request:
 $recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
--- phpscripts/registerBackendStudent.php
+++ phpscripts/registerBackendStudent.php
@@ -3,8 +3,28 @@
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
     error_reporting();*/
-    include("connectionTable.php");
-    lagCon("student","IA1vz6TNpdya6X8G");
+
+
+    $DATABASE_HOST = 'localhost';
+    $DATABASE_USER = 'student';
+    $DATABASE_PASS = 'IA1vz6TNpdya6X8G';
+    $DATABASE_NAME = 'virusnet';
+    // Try and connect using the info above.
+    $conMysqli = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+    $conMysqli->set_charset("utf8");
+    if (mysqli_connect_errno() ) {
+        // If there is an error with the connection, stop the script and display the error.
+        die ('Failed to connect to MySQL: ' . mysqli_connect_error());
+    }
+
+    $con = mysqli_connect("localhost", "root", "1337hackermangruppe09", "virusnet");
+    define( 'DB_HOST', 'localhost' ); // set database host
+    define( 'DB_USER', 'student' ); // set database user
+    define( 'DB_PASS', 'IA1vz6TNpdya6X8G' ); // set database password
+    define( 'DB_NAME', 'virusnet' ); // set database name
+    define( 'DISPLAY_DEBUG', false ); //display db errors?
+
+
     header("Access-Control-Allow-Origin: *");
     error_reporting();
 
